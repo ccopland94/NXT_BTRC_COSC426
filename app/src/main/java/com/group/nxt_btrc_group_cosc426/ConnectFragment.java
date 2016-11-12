9569e04063ba25abddab86026c3b0fd3c9eff97f
ccopland94 <ccopland@live.com>
2016-11-12 16:22:09 -0500

    Bluetooth works across both pages through Main Activity. Driving and connecting on separate with some button disabling and power adjust.

--- app/src/main/java/com/group/nxt_btrc_group_cosc426/ConnectFragment.java
+++ app/src/main/java/com/group/nxt_btrc_group_cosc426/ConnectFragment.java
@@ -14,6 +14,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -34,23 +35,7 @@ public class ConnectFragment extends Fragment {
     private ArrayList<BluetoothDevice> mBluetoothList;
     TextView cv_connectStatus;
     Button cv_btnDisconnect;
-    Button cv_btnDrive;
-    Button cv_btnLeft;
-    Button cv_btnRight;
-    Button cv_btnBack;
-    boolean cv_moveFlag = false;
-
-    // BT Variables
-    private BluetoothSocket cv_socket;
-    BluetoothDevice cv_bd;
-    final String CV_ROBOTNAME = "NXT";
-
-    // Detect BT connection status
-    private BroadcastReceiver cv_btMonitor = null;
-
-    // Data stream to/from NXT bluetooth
-    private InputStream cv_is = null;
-    private OutputStream cv_os = null;
+    Button mBTNconnect;
 
     public ConnectFragment() {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -72,14 +57,13 @@ public class ConnectFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_connect, container, false);
 
         cv_connectStatus = (TextView) view.findViewById(R.id.vv_connectStatus);
-        Button mBTNconnect = (Button) view.findViewById(R.id.BTN_connect);
+        mBTNconnect = (Button) view.findViewById(R.id.BTN_connect);
         cv_btnDisconnect = (Button) view.findViewById(R.id.vv_btnDisconnect);
-        cv_btnDrive = (Button) view.findViewById(R.id.vv_btnDrive);
-        cv_btnLeft = (Button) view.findViewById(R.id.vv_btnLeft);
-        cv_btnRight = (Button) view.findViewById(R.id.vv_btnRight);
-        cv_btnBack = (Button) view.findViewById(R.id.vv_btnBack);
 
-        cf_setupBTMonitor();
+        mBTNconnect.setEnabled(true);
+        cv_btnDisconnect.setEnabled(false);
+
+        //cf_setupBTMonitor();
         mBTNconnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,83 +75,16 @@ public class ConnectFragment extends Fragment {
         cv_btnDisconnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cf_disconnectNXT();
+                //disconnect robot from device
+                ((MainActivity)getActivity()).cf_disconnectNXT();
+
+                //enable connect, disable disconnect
+                mBTNconnect.setEnabled(true);
+                cv_btnDisconnect.setEnabled(false);
             }
         });
 
-        cv_btnDrive.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cv_moveFlag) {
-                    cf_moveMotor(0, 75, 0x20);
-                    cf_moveMotor(1, 75, 0x20);
-                }
-                else {
-                    cf_moveMotor(0, 75, 0x00);
-                    cf_moveMotor(1, 75, 0x00);
-                }
-                cv_moveFlag = !cv_moveFlag;
-            }
-        });
-
-        cv_btnLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cv_moveFlag) {
-                    cf_moveMotor(0, 75, 0x20);
-                    cf_moveMotor(1, -75, 0x20);
-                }
-                else {
-                    cf_moveMotor(0, 75, 0x00);
-                    cf_moveMotor(1, -75, 0x00);
-                }
-                cv_moveFlag = !cv_moveFlag;
-            }
-        });
-
-        cv_btnRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cv_moveFlag) {
-                    cf_moveMotor(0, -75, 0x20);
-                    cf_moveMotor(1, 75, 0x20);
-                }
-                else {
-                    cf_moveMotor(0, -75, 0x00);
-                    cf_moveMotor(1, 75, 0x00);
-                }
-                cv_moveFlag = !cv_moveFlag;
-            }
-        });
-
-        cv_btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cv_moveFlag) {
-                    cf_moveMotor(0, -75, 0x20);
-                    cf_moveMotor(1, -75, 0x20);
-                }
-                else {
-                    cf_moveMotor(0, -75, 0x00);
-                    cf_moveMotor(1, -75, 0x00);
-                }
-                cv_moveFlag = !cv_moveFlag;
-            }
-        });
         return view;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        getActivity().registerReceiver(cv_btMonitor,new IntentFilter("android.bluetooth.device.action.ACL_CONNECTED"));
-        getActivity().registerReceiver(cv_btMonitor,new IntentFilter("android.bluetooth.device.action.ACL_DISCONNECTED"));
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        getActivity().unregisterReceiver(cv_btMonitor);
     }
 
     private Dialog buildDialog() {
@@ -194,95 +111,19 @@ public class ConnectFragment extends Fragment {
                 .setAdapter(mPopupAdapter, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        // this toast shows selection is working
-                        cv_bd = mBluetoothList.get(i);
-                        Toast.makeText(getContext(),cv_bd.getAddress(),Toast.LENGTH_SHORT).show();
-                        cf_connectNXT();
-                        // Here code should be called to create socket to select device
-                        // Perhaps automatically change viewpager to Drive page if socket successful?
+                        //Set the bluetooth device in main activity to robot
+                        ((MainActivity)getActivity()).setDevice(mBluetoothList.get(i));
+
+                        //Toast.makeText(getContext(),cv_bd.getAddress(),Toast.LENGTH_SHORT).show();
+
+                        //connect the NXT (bind it to the bluetooth connection)
+                        ((MainActivity)getActivity()).cf_connectNXT();
+
+                        //disable connect, enable disconnect
+                        mBTNconnect.setEnabled(false);
+                        cv_btnDisconnect.setEnabled(true);
                     }
                 });
         return builder.create();
-    }
-
-    private void cf_connectNXT() {
-        try	{
-            if (cv_bd.getName().equalsIgnoreCase(CV_ROBOTNAME)) {
-                try {
-                    cv_socket = cv_bd.createRfcommSocketToServiceRecord(
-                            java.util.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
-                    cv_socket.connect();
-                }
-                catch (Exception e) {
-                    cv_connectStatus.setText("Error interacting with remote device [" +
-                            e.getMessage() + "]");
-                }
-            }
-        }
-        catch (Exception e) {
-           // cv_tvHello.setText("Failed in findRobot() " + e.getMessage());
-        }
-    }
-    private void cf_disconnectNXT() {
-        try {
-            cv_socket.close();
-            cv_is.close();
-            cv_os.close();
-            cv_connectStatus.setText(cv_bd.getName() + " is disconnect " );
-        } catch (Exception e) {
-            cv_connectStatus.setText("Error in disconnect -> " + e.getMessage());
-        }
-    }
-
-    private void cf_setupBTMonitor() {
-        cv_btMonitor = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (intent.getAction().equals(
-                        "android.bluetooth.device.action.ACL_CONNECTED")) {
-                    try {
-                        cv_is = cv_socket.getInputStream();
-                        cv_os = cv_socket.getOutputStream();
-                        cv_connectStatus.setText("Connect to " + cv_bd.getName() + " at " + cv_bd.getAddress());
-                    } catch (Exception e) {
-                        cf_disconnectNXT();
-                        cv_is = null;
-                        cv_os = null;
-                    }
-                }
-                if (intent.getAction().equals(
-                        "android.bluetooth.device.action.ACL_DISCONNECTED")) {
-                    cv_connectStatus.setText("Connection is broken");
-                }
-            }
-        };
-    }
-
-    private void cf_moveMotor(int motor,int speed, int state) {
-        try {
-            byte[] buffer = new byte[15];
-
-            buffer[0] = (byte) (15-2);			//length lsb
-            buffer[1] = 0;						// length msb
-            buffer[2] =  0;						// direct command (with response)
-            buffer[3] = 0x04;					// set output state
-            buffer[4] = (byte) motor;			// output 1 (motor B)
-            buffer[5] = (byte) speed;			// power
-            buffer[6] = 1 + 2;					// motor on + brake between PWM
-            buffer[7] = 0;						// regulation
-            buffer[8] = 0;						// turn ration??
-            buffer[9] = (byte) state; //0x20;					// run state
-            buffer[10] = 0;
-            buffer[11] = 0;
-            buffer[12] = 0;
-            buffer[13] = 0;
-            buffer[14] = 0;
-
-            cv_os.write(buffer);
-            cv_os.flush();
-        }
-        catch (Exception e) {
-            cv_connectStatus.setText("Error in MoveForward(" + e.getMessage() + ")");
-        }
     }
 }
--- app/src/main/java/com/group/nxt_btrc_group_cosc426/MainActivity.java
+++ app/src/main/java/com/group/nxt_btrc_group_cosc426/MainActivity.java
@@ -1,12 +1,20 @@
 package com.group.nxt_btrc_group_cosc426;
 
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
     private MyFragmentPagerAdapter mMyFragmentPagerAdapter;
     private ViewPager mViewPager;
+
+    // Detect BT connection status
+    private BroadcastReceiver cv_btMonitor = null;
+
+    // Data stream to/from NXT bluetooth
+    private InputStream cv_is = null;
+    private OutputStream cv_os = null;
+
+    // BT Variables
+    private BluetoothSocket cv_socket;
+    BluetoothDevice cv_bd;
+    final String CV_ROBOTNAME = "NXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.VP_viewpager);
         mViewPager.setAdapter(mMyFragmentPagerAdapter);
         mViewPager.setCurrentItem(1); // Connect page
+        cf_setupBTMonitor();
     }
 
     private static class MyFragmentPagerAdapter extends FragmentPagerAdapter {
@@ -59,6 +80,103 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return NUM_ITEMS;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(cv_btMonitor,new IntentFilter("android.bluetooth.device.action.ACL_CONNECTED"));
+        registerReceiver(cv_btMonitor,new IntentFilter("android.bluetooth.device.action.ACL_DISCONNECTED"));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(cv_btMonitor);
+    }
+
+    private void cf_setupBTMonitor() {
+        cv_btMonitor = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(
+                        "android.bluetooth.device.action.ACL_CONNECTED")) {
+                    try {
+                        cv_is = cv_socket.getInputStream();
+                        cv_os = cv_socket.getOutputStream();
+                        //cv_connectStatus.setText("Connect to " + cv_bd.getName() + " at " + cv_bd.getAddress());
+                    } catch (Exception e) {
+                        cf_disconnectNXT();
+                        cv_is = null;
+                        cv_os = null;
+                    }
+                }
+                if (intent.getAction().equals(
+                        "android.bluetooth.device.action.ACL_DISCONNECTED")) {
+                    //cv_connectStatus.setText("Connection is broken");
+                }
+            }
+        };
+    }
+    public void setDevice(BluetoothDevice device)
+    {
+        this.cv_bd = device;
+    }
+    public void cf_connectNXT() {
+        try	{
+            if (cv_bd.getName().equalsIgnoreCase(CV_ROBOTNAME)) {
+                try {
+                    cv_socket = cv_bd.createRfcommSocketToServiceRecord(
+                            java.util.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+                    cv_socket.connect();
+                }
+                catch (Exception e) {
+                    //cv_connectStatus.setText("Error interacting with remote device [" +
+                            //e.getMessage() + "]");
+                }
+            }
+        }
+        catch (Exception e) {
+            // cv_tvHello.setText("Failed in findRobot() " + e.getMessage());
+        }
+    }
+    public void cf_disconnectNXT() {
+        try {
+            cv_socket.close();
+            cv_is.close();
+            cv_os.close();
+            //cv_connectStatus.setText(cv_bd.getName() + " is disconnect " );
+        } catch (Exception e) {
+            //cv_connectStatus.setText("Error in disconnect -> " + e.getMessage());
+        }
+    }
+
+    public void cf_moveMotor(int motor,int speed, int state) {
+        try {
+            byte[] buffer = new byte[15];
+
+            buffer[0] = (byte) (15-2);			//length lsb
+            buffer[1] = 0;						// length msb
+            buffer[2] =  0;						// direct command (with response)
+            buffer[3] = 0x04;					// set output state
+            buffer[4] = (byte) motor;			// output 1 (motor B)
+            buffer[5] = (byte) speed;			// power
+            buffer[6] = 1 + 2;					// motor on + brake between PWM
+            buffer[7] = 0;						// regulation
+            buffer[8] = 0;						// turn ration??
+            buffer[9] = (byte) state; //0x20;					// run state
+            buffer[10] = 0;
+            buffer[11] = 0;
+            buffer[12] = 0;
+            buffer[13] = 0;
+            buffer[14] = 0;
+
+            cv_os.write(buffer);
+            cv_os.flush();
+        }
+        catch (Exception e) {
+            //cv_connectStatus.setText("Error in MoveForward(" + e.getMessage() + ")");
         }
     }
 }
